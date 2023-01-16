590ee9d5137c84704bff445c90bec6057bdd5228
Ahmed Kamal <dabourdabour28@gmail.com>
2023-01-16 11:03:55 +0200

    edit migration file name

--- database/migrations/2023_01_16_082022_create_course__student_table.php
+++ database/migrations/2023_01_16_082022_create_course__student_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('course__students', function (Blueprint $table) {
+        Schema::create('course__student', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('course__students');
+        Schema::dropIfExists('course__student');
     }
 };
